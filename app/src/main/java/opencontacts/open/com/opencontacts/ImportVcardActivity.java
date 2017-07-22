18faa157d476c2920d76b7585b1b0666d3b10fd3
Sultanahamer <sultanahamer@gmail.com>
2017-07-22 18:15:22 +0530

    [Sultan] - Fixed missing call icon in contact details screen and implemented UI for vCard import

--- app/src/main/java/opencontacts/open/com/opencontacts/ImportVcardActivity.java
+++ app/src/main/java/opencontacts/open/com/opencontacts/ImportVcardActivity.java
@@ -6,12 +6,14 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
 
 import ezvcard.Ezvcard;
@@ -22,13 +24,21 @@ import opencontacts.open.com.opencontacts.orm.Contact;
 import opencontacts.open.com.opencontacts.orm.PhoneNumber;
 
 public class ImportVcardActivity extends AppCompatActivity {
-    private String TOTAL_NUMBER_OF_VCARDS = "total_vcards";
-    private String NUMBER_OF_VCARDS_IMPORTED_UNTIL_NOW = "number_of_vcards_imported_until_now";
-    private String FINAL_RESULT_OF_IMPORT = "final_result_of_import";
+    private final String PROGRESS_TOTAL_NUMBER_OF_VCARDS = "total_vcards";
+    private final String PROGRESS_NUMBER_OF_VCARDS_PROCESSED_UNTIL_NOW = "number_of_vcards_imported_until_now";
+    private final String PROGRESS_FINAL_RESULT_OF_IMPORT = "final_result_of_import";
+    private ProgressBar progressBarComponent;
+    private TextView textView_vCardsIgnored, textView_vCardsImported;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_import_vcard);
+        progressBarComponent = (ProgressBar) findViewById(R.id.progressBar_vCard_Import);
+        textView_vCardsIgnored = (TextView) findViewById(R.id.textview_vcards_ignored);
+        textView_vCardsImported = (TextView) findViewById(R.id.textview_vcards_imported);
+        progressBarComponent.setIndeterminate(false);
+        progressBarComponent.setProgress(0);
+        progressBarComponent.setVisibility(View.VISIBLE);
         Intent intent = getIntent();
         Uri uri = intent.getData();
         VCardParser parser = new VCardParser();
@@ -43,7 +53,7 @@ public class ImportVcardActivity extends AppCompatActivity {
                 context = (Context) params[1];
                 InputStream vcardInputStream = context.getContentResolver().openInputStream(uri);
                 List<VCard> vCards = Ezvcard.parse(vcardInputStream).all();
-                publishProgress(TOTAL_NUMBER_OF_VCARDS, vCards.size());
+                publishProgress(PROGRESS_TOTAL_NUMBER_OF_VCARDS, vCards.size());
                 int numberOfvCardsImported=0, numberOfCardsIgnored = 0;
                 for(VCard vcard : vCards){
                     FormattedName name = vcard.getFormattedName();
@@ -51,17 +61,12 @@ public class ImportVcardActivity extends AppCompatActivity {
                         numberOfCardsIgnored++;
                         continue;
                     }
-                    Contact contact = new Contact();
-                    contact.firstName = name.getValue();
-                    contact.lastName = "";
-                    contact.save();
-                    for(Telephone telephoneNumber : vcard.getTelephoneNumbers()){
-                        new PhoneNumber(telephoneNumber.getText(), contact).save();
-                    }
+                    save(name.getValue(), vcard.getTelephoneNumbers());
                     numberOfvCardsImported++;
-                    publishProgress(NUMBER_OF_VCARDS_IMPORTED_UNTIL_NOW, numberOfvCardsImported);
+                    publishProgress(PROGRESS_NUMBER_OF_VCARDS_PROCESSED_UNTIL_NOW, numberOfvCardsImported, numberOfCardsIgnored);
+                    Thread.sleep(100);
                 }
-                publishProgress(FINAL_RESULT_OF_IMPORT, numberOfvCardsImported, numberOfCardsIgnored);
+                publishProgress(PROGRESS_FINAL_RESULT_OF_IMPORT, numberOfvCardsImported, numberOfCardsIgnored);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
                 Toast.makeText(context, R.string.error_while_parsing_vcard_file, Toast.LENGTH_LONG).show();
@@ -74,6 +79,14 @@ public class ImportVcardActivity extends AppCompatActivity {
                 Toast.makeText(context, R.string.unexpected_error_happened, Toast.LENGTH_LONG).show();
             }
             return null;
+        }
+        private void save(String firstName, List<Telephone> telephoneNumbers){
+            Contact contact = new Contact(firstName, "");
+            contact.save();
+            for(Telephone telephoneNumber : telephoneNumbers){
+                new PhoneNumber(telephoneNumber.getText(), contact).save();
+            }
+
         }
 
         @Override
@@ -89,6 +102,23 @@ public class ImportVcardActivity extends AppCompatActivity {
         @Override
         protected void onProgressUpdate(Object[] values) {
             super.onProgressUpdate(values);
+            switch((String)values[0]) {
+                case PROGRESS_NUMBER_OF_VCARDS_PROCESSED_UNTIL_NOW:
+                    int imported = (Integer) values[1], ignored = (Integer) values[2];
+                    progressBarComponent.setProgress(imported + ignored);
+                    textView_vCardsImported.setText("Total cards imported: " + imported);
+                    textView_vCardsIgnored.setText("Total cards ignored: " + ignored);
+                    break;
+                case PROGRESS_FINAL_RESULT_OF_IMPORT:
+                    progressBarComponent.setProgress(progressBarComponent.getMax());
+                    textView_vCardsImported.setText("Total cards imported: " + values[1]);
+                    textView_vCardsIgnored.setText("Total cards ignored: " + values[2]);
+                    break;
+                case PROGRESS_TOTAL_NUMBER_OF_VCARDS:
+                    progressBarComponent.setMax((Integer) values[1]);
+                    break;
+
+            }
         }
 
         @Override
